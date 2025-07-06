59b53335992d84b268e1743ebdf5c70550f2aa98
Victor Paul Mallavalli <victormallavalli7@gmail.com>
2025-07-07 02:21:50 +0530

    Update perf_counter.hpp

--- simulator/include/perf_counter.hpp
+++ simulator/include/perf_counter.hpp
@@ -10,20 +10,15 @@ private:
     std::unordered_map<std::string, int> unit_usage;
 
 public:
-    // Increments the usage count of a specific compute unit (e.g., "CIM", "VEC")
     void count(const std::string& unit);
 
-    // Increments the global cycle counter
     void tick();
 
-    // Prints a performance report to stdout
     void report() const;
 
-    // Saves a performance report to a log file
     void report_to_file(const std::string& filename) const;
 
-    // Saves an energy estimation report to a log file
     void report_energy_to_file(const std::string& filename) const;
 };
 
-#endif // PERF_COUNTER_HPP
+#endif 
